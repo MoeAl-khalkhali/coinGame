9e91d868b9f599a20605ac5aa5dc3845f042fe69
mouhannadak <mouhannadak@mouhannads-imac>
2019-10-09 18:31:12 +1100

    Added Dialogue box for add player entry

--- CoinGame/src/controller/AddPlayerController.java
+++ CoinGame/src/controller/AddPlayerController.java
@@ -2,18 +2,36 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Collection;
+
+import javax.swing.JOptionPane;
+
+import model.GameEngineImpl;
+import model.SimplePlayer;
+import model.interfaces.Player;
 
 public class AddPlayerController implements ActionListener
 {
-	public AddPlayerController() {
+	private GameEngineImpl gameEngine;
+
+	public AddPlayerController(GameEngineImpl gameEngine) {
+		this.gameEngine = gameEngine;
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		System.out.println("Clicked");
-		mlm
+		String firstName = JOptionPane.showInputDialog("Name");
+		String pointsBalance = JOptionPane.showInputDialog("How Many Points Do you Have");
+		int balance = Integer.parseInt(pointsBalance);
+		Player players =  new SimplePlayer("1", firstName, balance);
+		gameEngine.addPlayer(players);
+
+		
+
+		
+		
 	}
 
 }
